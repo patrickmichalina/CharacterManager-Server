b7c9e09c8daeb9a7aa3531673b6a66100cc62644
Patrick Michalina <patrickmichalina@mac.com>
2015-10-16 22:13:42 -0700

    feat(model): updates to the character class

--- CharacterManager/Models/Character.cs
+++ CharacterManager/Models/Character.cs
@@ -14,9 +14,10 @@ namespace CharacterManager.Models
         /// The character's name. Preferably mystical and esoteric.
         /// </summary>
         public string Name { get; set; }
-        
+
         /// <summary>
-        /// In the World of Warcraft universe there are but two races... Or are there...?
+        /// A character's race is the most tangible and real aspect to their being.
+        /// This is unchangeable and dictates almost all aspects of what choices they cant make.
         /// </summary>
         public virtual Race Race { get; set; }
 
@@ -29,5 +30,10 @@ namespace CharacterManager.Models
         /// A loosely related group of races joined in fighting toward mutually beneficials goals of varying degrees.
         /// </summary>
         public virtual Faction Faction { get; set; }
+
+        /// <summary>
+        /// Logical delete allows for deleting/undeleting of characters.
+        /// </summary>
+        public bool IsDeleted { get; set; }
     }
 }
--- CharacterManager/Models/Race.cs
+++ CharacterManager/Models/Race.cs
@@ -1,6 +1,23 @@
 ﻿namespace CharacterManager.Models
 {
+    /// <summary>
+    /// In the World of Warcraft universe there are but two races... Or are there...?
+    /// </summary>
     public class Race
     {
+        /// <summary>
+        /// Name of the race
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Details the charactersistic of the race
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// A url pointing to an arbitrary image of the race
+        /// </summary>
+        public string ImageUrl { get; set; }
     }
 }
