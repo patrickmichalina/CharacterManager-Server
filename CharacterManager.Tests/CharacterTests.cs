bd505eaa5d8143aca842c9c11c5344a7ea230f69
Patrick Michalina <patrickmichalina@mac.com>
2015-10-16 23:35:09 -0700

    test(harness): stubbed out NUnit test project

---
+++ CharacterManager.Tests/CharacterTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CharacterManager.Models;
+
+namespace CharacterManager.Tests
+{
+    [TestClass]
+    public class CharacterTests
+    {
+        private static Character _systemUnderTest;
+
+        [ClassInitialize]
+        public static void BeforeEachTest(TestContext testContext)
+        {
+            _systemUnderTest = new Character
+            {
+                Name = "Aleron",
+                Class = new Class { },
+                Faction = new Faction { },
+                Race = new Race { },
+                IsDeleted = false
+            };
+        }
+
+        [ClassCleanup]
+        public static void AfterEachTest()
+        {
+            _systemUnderTest = null;
+        }
+
+        [TestMethod]
+        public void PropertiesShouldSetAndGet()
+        {
+            Assert.AreEqual("Aleron", _systemUnderTest.Name);
+            ExtendedAssert.AreEqualByJson(new Faction { }, _systemUnderTest.Faction);
+            ExtendedAssert.AreEqualByJson(new Class { }, _systemUnderTest.Class);
+        }
+
+        
+    }
+}
---
+++ CharacterManager.Tests/ExtendedAssert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CharacterManager.Tests
+{
+    public static class ExtendedAssert
+    {
+        /// <summary>
+        /// Quick and simple way to text two objects without having to iterate over properties manually
+        /// </summary>
+        /// <param name="expected"></param>
+        /// <param name="actual"></param>
+        public static void AreEqualByJson(object expected, object actual)
+        {
+            var expectedJson = JsonConvert.SerializeObject(expected);
+            var actualJson = JsonConvert.SerializeObject(actual);
+            Assert.AreEqual(expectedJson, actualJson);
+        }
+    }
+}
