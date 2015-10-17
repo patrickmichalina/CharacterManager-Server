b5f2c4c29580c078f10a39c56903522d295bceec
Patrick Michalina <patrickmichalina@mac.com>
2015-10-17 00:58:43 -0700

    feat(database): added context stub

---
+++ CharacterManager/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace CharacterManager.Models
+{
+    /// <summary>
+    /// Entity Framework code first mappings of models to sql store
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        /// <summary>
+        /// Character entities in the database
+        /// </summary>
+        public DbSet<Character> Characters { get; set; }
+    }
+}
